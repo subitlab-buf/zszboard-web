99bde04f11fe092918a0360e9bd36486f8b3f821
C191239 <zsz2007314zsz@sina.com>
2023-10-10 11:00:25 +0800

    network

---
+++ scripts/network.js
@@ -0,0 +1,65 @@
+function postQuestion(name,info,email) {
+    const req=new XMLHttpRequest();
+    const content=JSON.stringify({
+        "name":name,
+        "info":info,
+        "email":email
+    });
+    req.open("POST","/questions/new",true);
+    req.setRequestHeader("content-type","application/json");
+    req.onreadystatechange(function(){
+        if(req.readyState==4){
+            if(req.status==200){
+                const res=JSON.parse(req.responseText);
+                if(res.code==10000){
+                    //Display animation
+                }else{
+                    //Display retry button
+                }
+            }else{
+                //Display retry button
+            }
+        }
+    });
+    req.send(content);
+}
+function postPaper(name,info,email){
+    const req=new XMLHttpRequest();
+    const content=JSON.stringify({
+        "name":name,
+        "info":info,
+        "email":email
+    });
+    req.open("POST","/paper/post",true);
+    req.setRequestHeader("content-type","application/json");
+    req.onreadystatechange(function(){
+        if(req.readyState==4){
+            if(req.status==200){
+                const res=JSON.parse(req.responseText);
+                if(res.code==10000){
+                    //Display animation
+                }else{
+                    //Display retry button
+                }
+            }else{
+                //Display retry button
+            }
+        }
+    });
+    req.send(content);
+}
+function getPaper(){
+    const req=new XMLHttpRequest();
+    req.open("GET","/paper/get",true);
+    req.onreadystatechange(function(){
+        if(req.readyState==4){
+            if(req.status==200){
+                const res=JSON.parse(req.responseText);
+                //Process paper
+            }else{
+                //Display retry button
+            }
+        }
+    });
+    req.send();
+}
